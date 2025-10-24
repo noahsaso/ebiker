0565a7c4c0b48371edf07bb730cc16bd2788c8f9
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-10-24 11:27:49 -0400

    plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Plausible } from "@/components/Plausible";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <Plausible />
       </body>
     </html>
   );
